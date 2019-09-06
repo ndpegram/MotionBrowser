01b2b27fee05bcb3c283c3a5b2a5fc7bfc975b93
ndpegram <ndpegram@gmail.com>
2019-09-06 18:02:22 +0800

    Use callback to show events on initial display. (Set things up so can use callback on date change.)

--- main.php
+++ main.php
@@ -3,16 +3,15 @@
  * Code for display of main viewer window
  * 
  */
-require_once $_SESSION['root_dir'] . '/classes/eventDay.php';
-require_once $_SESSION['root_dir'] . '/classes/eventDayFormatter.php';
-$day = new eventDay();
 
 ?>
 <script type="text/javascript" src="./js/main.js"></script>
 
 <div class="main">
 
-<?php echo (eventDayFormatUtils::formatEventDay(formatUtils::FORMAT_HTML, $day)); ?>
+<?php 
+require_once $_SESSION['root_dir'] . '/callbacks/getEvents.php';
+?>
 
 </div>
 
