110bd03b836ba6435b90a37c6d4b2509758e42af
ndpegram <ndpegram@gmail.com>
2019-09-15 13:49:41 +0800

    Class to consolidate web and file access to event file data. Extends SplFileInfo.

---
+++ classes/eventFileInfo.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Adds the parallel URL access set up in the web server. 
+ * 
+ * The setup should be done so that the files in the motion directory are
+ * able to be accessed from the root of the MotionBrowser directory via the URL
+ * '/video'.
+ * 
+ * For example, if the files are in /var/lib/motion/ and MotionBrowser is hosted
+ * at security.short.url.com then the videos and images should be accessible via
+ * security.short.url.com/video/
+ * 
+ * @author nigel
+ */
+class eventFileInfo extends SplFileInfo {
+    /** The path from the root to the images and videos. */
+    private CONST URL_PATH = '/video' ;
+    /** @var String URL to access the file. */
+    private $URL ;
+    
+    public function __construct(string $file_name): \SplFileInfo {
+        parent::__construct($file_name);
+        $this->setURL() ;
+    }
+    
+    private function setURL() {
+        $URL = self::getVideoBaseURL() ;
+        $URL .= $this->getBasename() ;
+        $this->URL = $URL ;
+    }
+    
+    public function getURL() {
+        return ($this->URL) ;
+    }
+    
+    private static function getVideoBaseURL() : string {
+        $url = parse_url(filter_input(INPUT_SERVER, 'HTTP_REFERER')) ;
+        $base = $url[PHP_URL_SCHEME].$url[PHP_URL_HOST] ;
+        return ($base) ;
+    }
+
+}
