19c92b74a9f185b31cc0a029f4e1bec27d3e29c2
ndpegram <ndpegram@gmail.com>
2020-02-20 18:17:15 +0800

    Add code to call disk cleaner before data displayed.

--- main.php
+++ main.php
@@ -8,8 +8,15 @@
 <script type="text/javascript" src="./js/main.js"></script>
 
 <div class="main">
-
+    
 <?php 
+//TODO: add code to check if over disk quota and delete days until not. See deleteOldestDay in old index.php:180. 
+// Perhaps use callback to run once document loaded and then refresh disk usage display.
+// 
+// TODO: TEST THIS!
+// Clean disk before displaying today's info.
+new diskCleaner() ;
+
 require_once $_SESSION['root_dir'] . '/callbacks/getEvents.php';
 ?>
 
